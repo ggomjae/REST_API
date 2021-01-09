9aab6b9cb5737f5ff148363df2ee54b58400408c
ggomjae <ggomjae@naver.com>
2021-01-10 01:30:45 +0900

    Image vuetify binding, textarea input block

--- src/fronted/src/components/TodoPost.vue
+++ src/fronted/src/components/TodoPost.vue
@@ -1,12 +1,97 @@
 <template>
   <v-main>
-    post
-
     <v-container fluid>
-      container
-      <!-- If using vue-router -->
-      <router-view></router-view>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="Outlined textarea"
+            v-bind:value="email"
+            disabled
+          ></v-textarea>
+        </v-col>
+      </v-row>
     </v-container>
+    <v-card
+      :loading="loading"
+      class="mx-auto my-12"
+      max-width="374"
+    >
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+
+      <v-img
+        height="250"
+        :src="img"
+      ></v-img>
+
+      <v-card-title>Cafe Badilico</v-card-title>
+
+      <v-card-text>
+        <v-row
+          align="center"
+          class="mx-0"
+        >
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ml-4">
+            4.5 (413)
+          </div>
+        </v-row>
+
+        <div class="my-4 subtitle-1">
+          $ • Italian, Cafe
+        </div>
+
+        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-title>Tonight's availability</v-card-title>
+
+      <v-card-text>
+        <v-chip-group
+          v-model="selection"
+          active-class="deep-purple accent-4 white--text"
+          column
+        >
+          <v-chip>5:30PM</v-chip>
+
+          <v-chip>7:30PM</v-chip>
+
+          <v-chip>8:00PM</v-chip>
+
+          <v-chip>9:00PM</v-chip>
+        </v-chip-group>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn
+          color="deep-purple lighten-2"
+          text
+          @click="reserve"
+        >
+          Reserve
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-main>
 </template>
 
@@ -14,48 +99,7 @@
   export default {
     data () {
       return {
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
+        img : "https://cdn.vuetifyjs.com/images/cards/cooking.png"
       }
     },
   }
