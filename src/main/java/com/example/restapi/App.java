b8986fc8338546abd2fd975c2f28ee9834467bba
ggomjae <ggomjae@naver.com>
2021-01-13 00:36:39 +0900

    check origin

--- src/main/java/com/example/restapi/App.java
+++ src/main/java/com/example/restapi/App.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//
+
 @EnableJpaAuditing // 이 어노테이션을 넣지 않으면 BaseTimeEntity가 안먹힘.
 @SpringBootApplication
 public class App {
